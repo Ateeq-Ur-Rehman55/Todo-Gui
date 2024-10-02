c4d23db09e4c81ab71afa0636b2a5a46d4bcfde2
Ateeq Ur Rehman <Ateeq-Ur-Rehman55>
2024-10-02 16:25:54 +0500

    last commit

--- public/script.js
+++ public/script.js
@@ -1,18 +1,25 @@
 // let new_data;
+
 // let new_id;
 let del_id;
 let update_id;
 let old_todo;
+let is_edit = false;
+let updating = false;
+if (window.location.reload)
+{
+    userInput.value = ""
+}
 fetch('http://localhost:3000/api/courses')                   //fetching data
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
         // body select to display data there
         const body = document.querySelector('p')
         for (let i = 0; i < data.length; i++) {
 
             //  adding todo data in p tag
             let div = document.createElement('div')
-            div.className = "list-group-item text-capitalize d-flex justify-content-between my-2"
+            div.id = "container";
             let p = document.createElement('p')
             p.className = "text"
             p = (`${data[i]['name']}`)
@@ -31,31 +38,42 @@ fetch('http://localhost:3000/api/courses')                   //fetching data
             update_btn.id = `${data[i]['id']}`
             // updating value
             update_btn.addEventListener('click', async (e) => {
-                // const usupdate = document.getElementById(update_input.id).value
-                update_id = e.originalTarget.id;
-                for (let j = 0; j < data.length; j++) {
-                    if (update_id == data[j]['id']) {
-                        old_todo = data[j]['name']
-                        console.log(`you are updating ${data[j]["name"]}`)
+                if (updating == true) {
+                    alert("Already updating a todo, submit it first and try agian...")
+                } else {
+                    updating = true;
+                    is_edit = true;
+                    // const usupdate = document.getElementById(update_input.id).value
+                    update_id = e.originalTarget.id;
+                    for (let j = 0; j < data.length; j++) {
+                        if (update_id == data[j]['id']) {
+                            old_todo = data[j]['name']
+                            console.log(`you are updating ${data[j]["name"]}`)
+                        }
                     }
+                    // send new value to the input field
+                    let updateInput = document.getElementById('userInput')
+                    updateInput.value = old_todo;
+                    // remove div
+                    let removeElmnt = document.getElementById('container')
+                    removeElmnt.remove()
                 }
+                // submit btn again...
+
                 // remove the old value
-                const requestOptions = {
-                    method: "DELETE",
-                    redirect: "follow"
-                };
+                // const requestOptions = {
+                //     method: "DELETE",
+                //     redirect: "follow"
+                // };
 
-                fetch(`http://localhost:3000/api/courses/${update_id}`, requestOptions)
-                    .then((response) => response.text())
-                    .then((result) => console.log(result))
-                    .catch((error) => console.error(error));
-                setTimeout(() => {
-                    window.location.reload();
-                }, 100);
+                // fetch(`http://localhost:3000/api/courses/${update_id}`, requestOptions)
+                //     .then((response) => response.text())
+                //     .then((result) => console.log(result))
+                //     .catch((error) => console.error(error));
+                // setTimeout(() => {
+                //     window.location.reload();
+                // }, 100);
 
-                // send new value to the input field
-                let updateInput = document.getElementById('userInput')
-                updateInput.value = old_todo;
                 // console.log(usupdate + "  value dot")
                 // console.log(`update id = ${update_id}`)
                 // // api call 
@@ -96,6 +114,7 @@ fetch('http://localhost:3000/api/courses')                   //fetching data
                         .then((result) => console.log(result))
                         .catch((error) => console.error(error));
                 }
+                userInput.value = ""
                 setTimeout(() => {
                     window.location.reload();
                 }, 100);
@@ -112,6 +131,7 @@ fetch('http://localhost:3000/api/courses')                   //fetching data
                     .then((response) => response.text())
                     .then((result) => console.log(result))
                     .catch((error) => console.error(error));
+                userInput.value = ""
                 setTimeout(() => {
                     window.location.reload();
                 }, 100);
@@ -142,50 +162,80 @@ fetch('http://localhost:3000/api/courses')                   //fetching data
                 document.getElementById("submitBtn").click();
             }
         });
-        submitBtn.addEventListener('click', function () {
-            let div = document.createElement('div')
-            let id;
-            if (data.length < 1) {
-                id = 1;
-            }
-            else {
-                id = data[data.length - 1]['id'] + 1;
-            }
-            const myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
-            const raw = JSON.stringify({
-                "id": id,
-                "name": userInput.value
-            });
-            const requestOptions = {
-                method: "POST",
-                headers: myHeaders,
-                body: raw,
-                redirect: "follow"
-            };
-            fetch("http://localhost:3000/api/courses", requestOptions)
-                .then((response) => response.text())
-                .then((data) => {
-                    let div = document.createElement('div')
-                    div = (`id: ${data[i]['id']},  Todo: ${data[i]['name']}`)
-                    div.id = data[i]['id'];
-                    let br = document.createElement('br')
-                    body.append(div)
-                    body.append(br)
-                })
-                .catch((error) => console.error(error));
+        submitBtn.addEventListener('click', async (e) => {
+            if (userInput.value == "") {
+                alert("Write something and try again...")
+            } else if (is_edit == true) {
+                updating = false;
+                const myHeaders = new Headers();
+                myHeaders.append("Content-Type", "application/json");
+                // let inputId = document.getElementById(data[i]['id'])
+                // console.log(`value of new id ${data[i]['name']}`.value)
+                const raw = JSON.stringify({
+                    "name": userInput.value
+                });
+                const requestOptions = {
+                    method: "PUT",
+                    headers: myHeaders,
+                    body: raw,
+                    redirect: "follow"
+                };
+                fetch(`http://localhost:3000/api/courses/${update_id}`, requestOptions)
+                    .then((response) => response.text())
+                    .then((result) => console.log(result))
+                    .catch((error) => console.error(error));
+                userInput.value = ""
+                setTimeout(() => {
+                    window.location.reload();
+                }, 100);
+            } else if (is_edit == false) {
+                let div = document.createElement('div')
+                let id;
+                if (data.length < 1) {
+                    id = 1;
+                }
+                else {
+                    id = data[data.length - 1]['id'] + 1;
+                }
+                const myHeaders = new Headers();
+                myHeaders.append("Content-Type", "application/json");
+                const raw = JSON.stringify({
+                    "id": id,
+                    "name": userInput.value
+                });
+                const requestOptions = {
+                    method: "POST",
+                    headers: myHeaders,
+                    body: raw,
+                    redirect: "follow"
+                };
+                fetch("http://localhost:3000/api/courses", requestOptions)
+                    .then((response) => response.text())
+                    .then((data) => {
+                        let div = document.createElement('div')
+                        div = (`id: ${data[i]['id']},  Todo: ${data[i]['name']}`)
+                        div.id = data[i]['id'];
+                        let br = document.createElement('br')
+                        body.append(div)
+                        body.append(br)
+                    })
+                    .catch((error) => console.error(error));
 
-            div.id = id;
-            div = (`Refresh to see id: Todo: ${userInput.value}`)
-            let br = document.createElement('br')
-            body.append(div)
-            body.append(br)
-            userInput.value = ""
-            setTimeout(() => {
-                window.location.reload();
-            }, 50);
-        });
+                div.id = id;
+                div = (`Refresh to see id: Todo: ${userInput.value}`)
+                let br = document.createElement('br')
+                body.append(div)
+                body.append(br)
+                userInput.value = ""
+                setTimeout(() => {
+                    window.location.reload();
+                }, 50);
+            }
+        })
+    }).catch((err) => {
+        alert('Write something and try again...')
     })
+
 // const submitBtn = document.getElementById('submitBtn');
 // const userInput = document.getElementById('userInput');
 // const display = document.getElementById('display');
